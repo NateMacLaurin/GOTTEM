83da4a8c2e583ea5b00537a79845d03d49a451d0
NateMacLaurin <brow1982@umn.edu>
2021-02-10 10:01:19 -0600

    added assetType router to server

--- database.sql
+++ database.sql
@@ -8,3 +8,11 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE "" (
+	"id" SERIAL PRIMARY KEY,
+);
+
+CREATE TABLE "" (
+	"id" SERIAL PRIMARY KEY,
+);
