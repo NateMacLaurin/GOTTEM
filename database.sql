70528a8f2c0cb9745ecab74e63511e556df2d316
NateMacLaurin <brow1982@umn.edu>
2021-02-14 22:21:56 -0600

    added static values to user database

--- database.sql
+++ database.sql
@@ -6,13 +6,24 @@
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "password" VARCHAR (1000) NOT NULL,
+    "isAdmin" BOOLEAN default FALSE
+);
+
+INSERT INTO "user" ("username", "password", "isAdmin")
+VALUES
+('admin', 'p3LmANHQ$g', TRUE),
+('nate', 'p3LmANHQ$g', FALSE);
+
+
+CREATE TABLE "assets_master" (
+	"id" SERIAL PRIMARY KEY,
+    "type_id" 
 );
 
 CREATE TABLE "" (
 	"id" SERIAL PRIMARY KEY,
 );
 
-CREATE TABLE "" (
-	"id" SERIAL PRIMARY KEY,
-);
+DROP TABLE "user";
+
