d3a33247d5473a537962b8441f592997a02ac5b5
NateMacLaurin <brow1982@umn.edu>
2021-02-16 09:04:49 -0600

    camelCase corrections

--- src/components/ItemPage/ItemPage.jsx
+++ src/components/ItemPage/ItemPage.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 import { useParams } from "react-router-dom";
-import {Card, ListGroup} from 'react-bootstrap';
 import SearchForm from '../SearchForm/SearchForm';
 import ItemDetails from '../ItemDetails/ItemDetails';
 
