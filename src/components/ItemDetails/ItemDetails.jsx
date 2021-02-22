abf834536bbda402d2ce1925000bb27856baa2e8
NateMacLaurin <brow1982@umn.edu>
2021-02-22 06:05:56 -0600

    item detail style done

--- src/components/ItemDetails/ItemDetails.jsx
+++ src/components/ItemDetails/ItemDetails.jsx
@@ -1,26 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Card, ListGroup } from 'react-bootstrap';
-import {Paper, Button} from '@material-ui/core';
-import TitlePaper from '../TitlePaper/TitlePaper';
+import {Button, makeStyles, Card, CardContent, TextField, Grid, Select, FormControl, MenuItem, Typography, CardActions} from '@material-ui/core';
 import './ItemDetails.css';
 
 function ItemDetails(props) {
-
+       //constant
+    const defaultAsset = {
+        assetID: 1,
+        assetNumber: '',
+        domain_name: '',
+        ipv4: '',
+        mac_addr: '',
+        type_name: '',
+        type_id: 1,
+        location_name: '',
+        location_id: 1,
+        status_name: '',
+        status_id: 1    
+    }
+        //state
+    const [asset, setAsset] = useState(defaultAsset);
     const [isEditing, setIsEditing] = useState(false);
-        //asset editable fields
-    const [assetID, setAssetID] = useState(0);
-    const [assetName, setAssetName] = useState('NAME_HERE');
-    const [assetNumber, setAssetNumber] = useState(0);
-    //const [assetNumber, setAssetNumber] = useState(12345678);
-    const [assetIP, setAssetIP] = useState('123.456.789.123');
-    const [assetMAC, setAssetMAC] = useState('12-34-56-78-90-AB');
-    const [assetType, setAssetType] = useState(0);
-    const [assetLocation, setAssetLocation] = useState(0);
-    const [assetStatus, setAssetStatus] = useState(0);
-    const store = useSelector((store) => store);
+
         //hooks
+    const store = useSelector((store) => store);
     const dispatch = useDispatch();
+    const classes = useStyles();
 
     const handleEdit = () => {
         console.log(`Clicked Edit`);
@@ -35,119 +40,186 @@ function ItemDetails(props) {
 
     const handleSave = () => {
         console.log(`Clicked Save`);
-        dispatch({type: 'EDIT_ASSET', payload: {
-            id: assetID, 
-            assetNumber: assetNumber,
-            domain_name: assetName,
-            ipv4: assetIP, 
-            mac_addr: assetMAC
-          }
-        });
+
+        asset.assetNumber = parseInt(asset.assetNumber);
+        asset.type_id = parseInt(asset.type_id);
+        asset.location_id = parseInt(asset.location_id);
+        asset.status_id = parseInt(asset.status_id);
+
+        dispatch({type: 'EDIT_ASSET', payload: asset});
+
         setIsEditing(false);
     }
 
     useEffect(() => {
-        setAssetID(props.targetAsset[0]?.id);
-        setAssetNumber(props.targetAsset[0]?.assetNumber);
-        setAssetName(props.targetAsset[0]?.domain_name);
-        setAssetIP(props.targetAsset[0]?.ipv4);
-        setAssetMAC(props.targetAsset[0]?.mac_addr);
-        setAssetType(props.targetAsset[0]?.type_name);
-        setAssetLocation(props.targetAsset[0]?.loc_name);
-        setAssetStatus(props.targetAsset[0]?.status_name);
+
+        setAsset({
+            ...asset, 
+            assetID: props.targetAsset[0]?.id,
+            assetNumber: props.targetAsset[0]?.assetNumber,
+            domain_name: props.targetAsset[0]?.domain_name,
+            ipv4: props.targetAsset[0]?.ipv4,
+            mac_addr: props.targetAsset[0]?.mac_addr,
+            type_name: props.targetAsset[0]?.type_name,
+            location_name: props.targetAsset[0]?.loc_name,
+            status_name: props.targetAsset[0]?.status_name
+        })
     }, [])
 
     return (
-        <div className="itemCard">
-            <div className="inventoryPaperTitle">
-                <TitlePaper className="title" title={'Details'}/>
-            </div>
-            <Paper>
-        <Card key={assetID} stype={{width: '14rem'}}>
-            <Card.Body>
-            {isEditing ?
-            <>
-                <Card.Title>
-                    <input
-                        className="localNameField"
-                        value={assetName}
-                        onChange={(event) => setAssetName(event.target.value)}
-                    />
-                </Card.Title>
-                <Card.Subtitle>
-                    <input
-                        className="localNumberField"
-                        value={assetNumber}
-                        onChange={(event) => setAssetNumber(event.target.value)}
-                    />
-                </Card.Subtitle>
-                <Card.Subtitle>
-                    <label htmlFor="typeSelect">Type:</label>
-                    <select className="typeSelect" value={assetType} onChange={(event) => setAssetType(event.target.value)}>
-                        <option value='1'>Desktop PC</option>
-                        <option value='2'>Laptop Macintosh</option>
-                        <option value='3'>Laptop PC</option>
-                        <option value='4'>Phone</option>
-                    </select>
-                </Card.Subtitle>
-                <Card.Subtitle className="mb-2 text-muted">
-                    <label htmlFor="locationSelect">Location:</label>
-                    <select className="locationSelect" value={assetLocation} onChange={(event) => setAssetLocation(event.target.value)}>
-                        <option text='1'>Main Office 100 Big Chungus Dr</option>
-                        <option value='2'>Sattelite Office 110 Enterprise Pkwy</option>
-                        <option value='2'>New Branch 123 Main Street</option>
-                        <option value='4'>Work From Home</option>
-                    </select>
-                </Card.Subtitle>
-                <ListGroup variant="info">
-                    <ListGroup.Item>
-                        <input
-                            className="localIPField"
-                            value={assetIP}
-                            onChange={(event) => setAssetIP(event.target.value)}
+        <div className="itemCard" style={{margin: '1em'}}>
+            <Card className={classes.root}>
+                <CardContent>
+                {isEditing ?
+                    <form className={classes.root} noValidate onSubmit={handleSave}>
+                    <Grid container spacing={2} alignItems="stretch" justify="flex-start" direction="row" xs={12}>
+                        <Grid item xs={6}>
+                        <TextField
+                            id="assetNumberInput"
+                            type="number"
+                            label="Asset Number"
+                            required
+                            variant="outlined"
+                            value={asset.assetNumber}
+                            onChange={(event) => setAsset({ ...asset, assetNumber: event.target.value})}
                         />
-                    </ListGroup.Item>
-                    <ListGroup.Item>
-                        <input
-                            className="localMACField"
-                            value={assetMAC}
-                            onChange={(event) => setAssetMAC(event.target.value)}
+                    </Grid>
+                    <Grid item xs={6}>
+                        <TextField
+                            id="DomainNameInput"
+                            type="text"
+                            label="Domain Name"
+                            required
+                            variant="outlined"
+                            value={asset.domain_name}
+                            onChange={(event) => setAsset({ ...asset, domain_name: event.target.value})}
                         />
-                    </ListGroup.Item>
-                    <ListGroup.Item>
-                        <label htmlFor="statusSelect">Status:</label>
-                        <select className="statusSelect" value={assetStatus} onChange={(event) => setAssetStatus(event.target.value)}>
-                            <option value='1'>In Use</option>
-                            <option value='2'>In Inventory</option>
-                            <option value='3'>Awaiting Repair</option>
-                            <option value='4'>Ordered</option>
-                            <option value='5'>Retired</option>
-                        </select>        
-                    </ListGroup.Item>
-                </ListGroup>
-                <Button type="submit" onClick={handleSave} variant="contained" color="primary">Save</Button>
-            </>
-            : 
-            <>
-                <Card.Title>{assetName}</Card.Title>
-                <Card.Subtitle>{assetNumber}</Card.Subtitle>
-                <Card.Subtitle>{assetType}</Card.Subtitle>
-                <Card.Subtitle className="mb-2 text-muted">{assetLocation}</Card.Subtitle>
-                <ListGroup variant="info">
-                    <ListGroup.Item>{assetIP}</ListGroup.Item>
-                    <ListGroup.Item>{assetMAC}</ListGroup.Item>
-                    <ListGroup.Item>{assetStatus}</ListGroup.Item>
-                </ListGroup>
-                <Button type="submit" variant="contained" color="primary" onClick={handleEdit}>Edit</Button>
-            </>
+                        </Grid>
+                        <Grid item xs={6}>
+                        <TextField
+                            id="iPInput"
+                            type="text"
+                            label="IP Address"
+                            required
+                            variant="outlined"
+                            value={asset.ipv4}
+                            onChange={(event) => setAsset({ ...asset, ipv4: event.target.value})}
+                        />
+                        </Grid>
+                        <Grid item xs={6}>
+                        <TextField
+                            id="mACInput"
+                            type="text"
+                            label="MAC Address"
+                            required
+                            variant="outlined"
+                            value={asset.mac_addr}
+                            onChange={(event) => setAsset({ ...asset, mac_addr: event.target.value })}
+                        />
+                        </Grid>
+                    </Grid>
+                    <Grid container spacing={2} alignItems="stretch" justify="space-evenly" direction="column">
+                        <Grid item>
+                        <FormControl>
+                            <Select 
+                            id="typeSelect" 
+                            type="number"
+                            label="Asset Type"
+                            required
+                            value={asset.type_id} 
+                            onChange={(event) => setAsset({ ...asset, type_id: event.target.value })}
+                            >
+                            <MenuItem value={1}>Desktop PC</MenuItem>
+                            <MenuItem value={2}>Laptop PC</MenuItem>
+                            <MenuItem value={3}>Laptop Mac</MenuItem>
+                            <MenuItem value={4}>Tablet</MenuItem>
+                            <MenuItem value={5}>Phone</MenuItem>
+                            </Select>
+                        </FormControl>
+                        </Grid>
+                        <Grid item>
+                        <FormControl>
+                            <Select
+                            id="locationSelect" 
+                            type="number"
+                            label="Location Address"
+                            required
+                            value={asset.location_id} 
+                            onChange={(event) => setAsset({ ...asset, location_id: event.target.value })}
+                            >
+                            <MenuItem value={1}>Main Office 100 Big Chungus Dr</MenuItem>
+                            <MenuItem value={2}>Sattelite Office 110 Enterprise Pkwy</MenuItem>
+                            <MenuItem value={3}>New Branch 123 Main Street</MenuItem>
+                            <MenuItem value={4}>Work From Home</MenuItem>
+                            </Select>
+                        </FormControl>
+                        </Grid>
+                        <Grid item>
+                        <FormControl>
+                            <Select 
+                            id="statusSelect"
+                            type="number"
+                            label="Status"
+                            required
+                            value={asset.status_id} 
+                            onChange={(event) => setAsset({...asset, status_id: event.target.value })}
+                            >
+                                <MenuItem value={1}>In Use</MenuItem>
+                                <MenuItem value={2}>In Inventory</MenuItem>
+                                <MenuItem value={3}>Awaiting Repair</MenuItem>
+                                <MenuItem value={4}>Ordered</MenuItem>
+                                <MenuItem value={5}>Retired</MenuItem>
+                            </Select>
+                        </FormControl>
+                        </Grid>
+                    <Button type="submit" variant="contained" color="primary">Save</Button>
+                    {store.user.isAdmin ? <Button className="deleteButton" type="submit" variant="contained" color="secondary" onClick={handleDelete}>Delete</Button> : <></>}
+                    </Grid>
+                    </form>
+                : 
+                <>
+                    <Typography className={classes.title} color="textSecondary" gutterBottom>
+                    {asset.assetNumber}
+                    </Typography>
+                    <Typography variant="h5" component="h2">
+                    {asset.domain_name}
+                    </Typography>
+                    <Typography className={classes.pos} color="textSecondary">
+                    {asset.type_name}
+                    <br />
+                    {asset.status_name}
+                    </Typography>
+                    <Typography variant="body1" component="p">
+                    {asset.location_name}
+                    </Typography>
+                    <Typography variant="body2" component="p">
+                    {asset.ipv4}
+                    <br />
+                    {asset.mac_addr}
+                    </Typography>
+                    <CardActions>
+                        <Button type="submit" variant="contained" color="primary" onClick={handleEdit}>Edit</Button>
+                        {store.user.isAdmin ? <Button className="deleteButton" type="submit" variant="contained" color="secondary" onClick={handleDelete}>Delete</Button> : <></>}
+                    </CardActions>
+                </>
             }
-                {/*this will be a button to delete this item*/}
-                { store.user.isAdmin ? <Button className="deleteButton" type="submit" variant="contained" color="secondary" onClick={handleDelete}>Delete</Button> : <></>}
-          </Card.Body>
+            </CardContent>
         </Card>
-        </Paper>
+        
         </div>
     );
 }
 
 export default ItemDetails;
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      '& .MuiTextField-root': {
+        width: '30ch',
+      },
+      '& .MuiSelect-root': {
+        width: '30ch',
+      }
+      },
+    })
+  );
--- src/components/ItemPage/ItemPage.jsx
+++ src/components/ItemPage/ItemPage.jsx
@@ -3,6 +3,9 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from "react-router-dom";
 import SearchForm from '../SearchForm/SearchForm';
 import ItemDetails from '../ItemDetails/ItemDetails';
+import TitlePaper from '../TitlePaper/TitlePaper';
+import { Paper } from '@material-ui/core';
+import './ItemPage.css';
 
 function ItemPage(props) {
     //vars
@@ -11,18 +14,24 @@ function ItemPage(props) {
   id ? id=id : id=1;
     //hooks
   const dispatch = useDispatch();
-  const baseSearchFields = useSelector((store) => store.baseSearchFields);
   const targetAsset = useSelector((store) => store.masterAssetItem);
     //debug log
   console.log(`ID param of redirect: ${id}`);
 
   return (
-    <div>
-      <SearchForm baseSearchFields={baseSearchFields}/>
-      {targetAsset.length != 0 ? 
-        <ItemDetails targetAsset={targetAsset}/>:<h1>Search for an Item to Populate</h1>
-      }
-      {/*this will be a return button to return to inventory page*/}
+    <div className="itemContainer">
+      <div className="itemPaperTitle">
+          <TitlePaper className="title" title={'Selection'}/>
+      </div>
+      <div className="itemPaper">
+        <Paper elevation={3}>
+            <SearchForm />
+            {targetAsset.length != 0 ? 
+              <ItemDetails targetAsset={targetAsset}/>: 
+              <h1>Search for an Item to Populate</h1>
+            }
+        </Paper>
+      </div>
     </div>
   );
 }
